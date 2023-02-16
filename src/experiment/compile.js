87f994791fca32ff926692ce30313cab5e5b057c
Egon Firman <egon.firman@tokopedia.com>
2023-02-16 20:45:42 +0700

    updating the experiment

--- src/experiment/compile.js
+++ src/experiment/compile.js
@@ -6,7 +6,7 @@ const fs = require('fs')
 
 const intepreter = new ArithmeticInterpreter();
 
-function compile(filename) {
+function compile(filename, astOut, asmOut) {
 	const input = fs.readFileSync(filename).toString()
 	const tokenizer = new Tokenizer();
 	const tokenList = tokenizer.parse(input);
@@ -14,10 +14,10 @@ function compile(filename) {
 	const ast = parser.parse(tokenList);
 	const asm = gen(ast)
 
-	fs.writeFileSync('source.asm', asm)
-	fs.writeFileSync('source_ast.json', JSON.stringify(ast, null, 2))
+	fs.writeFileSync(astOut, JSON.stringify(ast, null, 2))
+	fs.writeFileSync(asmOut, asm)
 }
 
-compile('source.eg')
+compile('source.eg', 'source_ast.json', 'source.s')
 
 
